89dd97f55ee0486d13738383b32349c6831c8d05
NKDuy <kn145660@gmail.com>
2023-04-23 14:42:35 +0700

    fix branch name

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:khanhduy1407/kdu-grid-sample.git main:gh-pages
+git push -f git@github.com:khanhduy1407/kdu-grid-sample.git master:gh-pages
 
 cd -
